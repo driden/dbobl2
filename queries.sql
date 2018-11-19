a6c173db352cf3708871d7b2f1ecc15ca94ccb71
driden <776982+driden@users.noreply.github.com>
2018-11-19 18:07:20 -0300

    primer commit

---
+++ queries.sql
@@ -0,0 +1,35 @@
+-- Obtener la latitud y la longitud de los puntos limpios donde se depositaron artículos con el mayor peso, 
+-- considerando los artículos de tipo celular. 
+-- Si hay más de un punto limpio que cumple las condiciones, mostrarlos a todos.
+
+SELECT d.pesokg, pl.latitud, pl.longitud
+FROM (deposito d
+INNER JOIN PUNTOLIMPIO pl
+ON pl.nombrepunto = d.nombrepunto)
+INNER JOIN ARTICULO art
+ON art.nombre = d.nombre
+WHERE art.tipoarticulo = 'Celular'
+AND d.pesokg = (SELECT MAX(pesokg) FROM Deposito);
+
+
+-- Obtener los puntos limpios de Montevideo y Canelones donde se depositaron artículos de tipo laptop o televisores, 
+-- pero no ambos tipos. Tener en cuenta solo los depósitos de 2 a 5 kilos.
+
+SELECT *
+FROM (deposito d
+INNER JOIN puntolimpio pl
+ON d.nombrepunto = pl.nombrepunto)
+INNER JOIN articulo art
+ON art.nombre = d.nombre
+WHERE d.pesokg BETWEEN 2 AND 5
+AND pl.intendencia IN ('Montevideo', 'Canelones')
+AND 1 = (
+        SELECT COUNT(a1.tipoarticulo)
+        FROM DEPOSITO d1 
+        INNER JOIN ARTICULO a1 
+        ON d1.nombre = a1.nombre 
+        WHERE a1.tipoarticulo IN ('Television', 'Laptops')
+        and d1.nombrepunto = d.nombrepunto -- IMPORTANTE: Necesito usar el mismo deposito que por el que joineo
+        GROUP BY d1.nombrepunto
+        )
+        
